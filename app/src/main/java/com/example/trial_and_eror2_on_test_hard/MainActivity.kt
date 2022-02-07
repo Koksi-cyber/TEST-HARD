9c9351a995bd440b1f89002290d43b4a493347c1
Koksi-cyber <godswillekong4@gmail.com>
2022-02-07 10:21:23 +0100

    android dev

---
+++ app/src/main/java/com/example/trial_and_eror2_on_test_hard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.trial_and_eror2_on_test_hard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
