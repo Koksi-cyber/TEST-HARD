f8a2b5cb5057a3bba9a565affbfe122a53e63996
Koksi-cyber <godswillekong4@gmail.com>
2022-02-07 12:36:18 +0100

    android dev

--- app/src/main/java/com/example/trial_and_eror2_on_test_hard/MainActivity.kt
+++ app/src/main/java/com/example/trial_and_eror2_on_test_hard/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.trial_and_eror2_on_test_hard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 5000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler()
+            .postDelayed(
+                {
+                    startActivity(Intent(this, problempage::class.java))
+                    finish()
+                }, SPLASH_TIME_OUT
+            )
     }
 }
